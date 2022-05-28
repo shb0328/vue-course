190fa440fa44c39aef9e930b86cb146377be0894
hyebb <hyebb@ncsoft.com>
2022-05-28 09:41:08 +0900

    basic-assignment 4

---
+++ basics-assignment-4-problem/app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      inputClass: '',
+      paragraphIsVisible: true,
+      inputBackgroundColor: ''
+    };
+  },
+  computed: {
+    paraClasses() {
+      return {
+        user1: this.inputClass === 'user1', 
+        user2: this.inputClass === 'user2',
+        visible: this.paragraphIsVisible,
+        hidden: !this.paragraphIsVisible
+      };
+    }
+  },
+  methods : {
+    toggleParagraphVisiblility() {
+      this.paragraphIsVisible = !this.paragraphIsVisible;
+    }
+  }
+});
+
+app.mount('#assignment');
