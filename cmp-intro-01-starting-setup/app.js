e6c9de7f9f6ba5909a67f6eb7b7f3855e4f5da3a
hyebb <hyebb@ncsoft.com>
2022-05-30 19:09:18 +0900

    component

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [ // from db ...
         {
           id:'manuel', 
@@ -26,4 +25,34 @@ const app = Vue.createApp({
 
 });
 
+app.component('friend-contact', {
+  template: `
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone}}</li>
+        <li><strong>Email:</strong> {{ friend.email}}</li>
+      </ul>
+    </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id:'manuel', 
+        name:'Manuel Lorenz', 
+        phone:'01234 5678 991', 
+        email:'manuel@localhost.com'
+      }
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+}); //identifier, conflict object 
+//identifier : 사용자 정의 HTML 요소 이름. 내장 HTML element와 충돌을 피하기 위해서 반드시 '-' (dash) 를 포함한다.
+//conflict object : main app에 연결될 app의 개체 정보. capsulation. main app과 통신하지 않음, 자체 template이 필요하다
 app.mount('#app');
