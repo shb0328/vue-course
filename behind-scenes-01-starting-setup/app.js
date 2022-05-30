8af22c23e4057b87fb50be9fe90903d0251336b4
hyebb <hyebb@ncsoft.com>
2022-05-30 14:10:47 +0900

    - vue reactive deep dive - one app vs. multiple app

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -16,3 +16,36 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      ilove:'pizza'
+    };
+  }
+});
+
+app2.mount('#app2');
+
+// vue deep dive with vanilla js
+// vanilla js is not reactive
+
+const data = {
+  message: 'hello',
+  longMessage: 'hello!!! world!!!'
+}
+
+const handler = {
+  set(target, key, value) {
+    if(key === 'message') {
+      target.longMessage = value + 'world!';
+    }
+    target.message = value;
+  }
+}
+
+const proxy = new Proxy(data, handler); // target = data
+
+proxy.message = 'hello vue!'
+
+console.log(proxy.longMessage);
