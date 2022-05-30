b5bac4b9f358ec4b6e352b63e11841d4d0d6f2ac
hyebb <hyebb@ncsoft.com>
2022-05-30 20:40:39 +0900

    set FriendContact Component

--- vue-cli-project/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-project/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,6 +1,32 @@
-<template></template>
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><string>Phone:</string>{{ friend.phone }}</li>
+      <li><string>Email:</string>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
 
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+          id:'manuel',
+          name:'Manuel Lorenz',
+          phone:'0123 456',
+          email:'man@localhost.com'
+        }
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
 </script>
