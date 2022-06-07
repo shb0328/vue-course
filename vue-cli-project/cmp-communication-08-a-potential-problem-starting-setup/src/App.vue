5c2345387bbd097b7e329cb11b6d938cefdd69ab
hyebb <hyebb@ncsoft.com>
2022-06-07 23:57:55 +0900

    change the way we use provide (working on one and the same object in memory)

--- vue-cli-project/cmp-communication-08-a-potential-problem-starting-setup/src/App.vue
+++ vue-cli-project/cmp-communication-08-a-potential-problem-starting-setup/src/App.vue
@@ -32,30 +32,26 @@ export default {
       activeTopic: null,
     };
   },
-  provide: {
-    topics: [
-        {
-          id: 'basics',
-          title: 'The Basics',
-          description: 'Core Vue basics you have to know',
-          fullText:
-            'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
-        },
-        {
-          id: 'components',
-          title: 'Components',
-          description:
-            'Components are a core concept for building Vue UIs and apps',
-          fullText:
-            'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
-        },
-      ],
-  },  
+  provide() {
+    return {
+      topics: this.topics
+    };
+  },
   methods: {
     activateTopic(topicId) {
       this.activeTopic = this.topics.find((topic) => topic.id === topicId);
     },
   },
+  mounted() { // test
+    setTimeout(() => {
+      this.topics.push({
+        id: 'events',
+        title: 'Events',
+        description: 'Events are important in Vue',
+        fullText: 'Events allow you to trigger code on demand!'
+      });
+    }, 3000);
+  }
 };
 </script>
 
